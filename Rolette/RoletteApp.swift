1498ef86b7e028460429a078a4d07cb855830c75
Rob Miguel <robmiguel2@gmail.com>
2024-12-15 22:26:17 -0700

    Initial Commit

---
+++ Rolette/RoletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoletteApp.swift
+//  Rolette
+//
+//  Created by Rob Miguel on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct RoletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
